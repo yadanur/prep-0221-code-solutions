ba742795dd7e5cd9d79c7dfb5ed3d9057cc6b9c9
Yusuf Adanur <y.adanur@gmail.com>
2021-02-27 13:48:09 -0800

    completed for loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+// create your loops here.
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (var x = 0; x < 20; x += 2) {
+  console.log(x);
+}
+
+for (var time = 100; time > 0; time--) {
+  console.log('Time till explosion: ' + time + '!');
+}
